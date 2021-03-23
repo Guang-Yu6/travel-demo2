8e9a57f3f5aa35158203cb3aa2478a3722ddf113
Guang-Yu6 <everyting-day@foxmail.com>
2021-03-23 21:29:24 +0800

    lunbo

---
+++ README.md
@@ -0,0 +1,60 @@
+# Vue2.5开发去哪儿网App
+
+> 从基础到项目，从零开发去哪儿网App
+
+## 在开发过程中遇到的一些困难及解决方案
+
+### 1.Vue项目中使用iconfontcss，无法显示正确图标
+
+1).文件存放路径为
+  
+![](http://oriq21dog.bkt.clouddn.com/bloc/2018-05-30-QQ20180530-153237.png)  
+
+2).iconfont.css里面的路径设置
+![](http://oriq21dog.bkt.clouddn.com/bloc/2018-05-30-QQ20180530-153358.png)
+
+3).iconfont的使用:class为iconfont,内容为16进制  
+![](http://oriq21dog.bkt.clouddn.com/bloc/2018-05-30-QQ20180530-153547.png)
+
+### 2.轮播图padding-bottom的值 200/750 = 26.67
+
+![](http://oriq21dog.bkt.clouddn.com/bloc/2018-05-30-QQ20180530-163433.png)
+
+### 3.轮播图索引点的颜色设置 
+ 
+```
+.wrapper >>> .swiper-pagination-bullet-active
+    background : #fff
+```
+### 4.一像素边框问题解决
+
+引入border.css,在用到的地方使用 `class="border-bottom"`
+
+### 5.端口号被占用或者非正常退出时端口号改变的问题
+
+```
+我配置的端口号为 8089
+
+1. lsof -i tcp:端口号
+2. kill -9 被占用的pid
+3. npm run start
+
+```
+![](http://oriq21dog.bkt.clouddn.com/bloc/2018-06-01-QQ20180601-093059.png)
+
+### 6.click突然失效或click点击区域范围变化
+```
+1. 检查代码  
+2. 切换几次浏览器中模拟器的大小
+```
+![](http://oriq21dog.bkt.clouddn.com/bloc/2018-06-03-QQ20180604-071041%402x.png)
+
+### 7.Detail页面网络数据只请求一次
+![](http://oriq21dog.bkt.clouddn.com/bloc/2018-06-05-QQ20180605-114315%402x.png)
+
+```
+在App.vue的keep-alive中添加exclude="Detail"
+```
+
+
+![](http://oriq21dog.bkt.clouddn.com/bloc/2018-06-05-QQ20180605-114522.png)
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" :src="bannerImg"/>
+      <div class="banner-info">
+        <div class="banner-title">
+          {{sightName}}
+        </div>
+        <div class="banner-number">
+          <span class="iconfont banner-icon">&#xe63b;</span>
+          99
+        </div>
+      </div>
+    </div>
+    <fade-animation>
+      <common-gallary :imgs="bannerImgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+    </fade-animation>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/Fade'
+export default {
+  name: 'DetailBanner',
+  components: {
+    CommonGallary,
+    FadeAnimation
+  },
+  props: {
+    sightName: '',
+    bannerImg: '',
+    bannerImgs: ''
+  },
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1601/3d/3dc7f222d6bca6a790.img.jpg_r_800x800_27e89341.jpg', 'http://img1.qunarzz.com/sight/p0/1601/14/14ada37b4792e56b90.img.jpg_r_800x800_b117e3e5.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position: relative
+    overflow : hidden
+    height : 0
+    padding-bottom : 55%
+    .banner-img
+      width: 100%
+    .banner-info
+      display : flex
+      position absolute
+      left:0
+      right:0
+      bottom:0
+      line-height:0.6rem
+      color : #fff
+      background-image : linear-gradient(top,rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.8))
+      .banner-title
+        font-size : .32rem
+        padding : 0 .2rem
+        flex: 1
+      .banner-number
+        margin-top : .14rem
+        padding:0 .4rem 0 .4rem
+        font-size : .24rem
+        height : .34rem
+        line-height : .34rem
+        border-radius : .2rem
+        background : rgba(0, 0, 0, .8)
+        .banner-icon
+          font-size : .26rem
+</style>
